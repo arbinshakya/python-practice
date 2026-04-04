627f46151873f76344bf4efd10839af225eb4e6c
Arbin <arbinshakya123@gmail.com>
2026-04-04 23:42:00 +0545

    comments added

--- oop/oop.py
+++ oop/oop.py
@@ -1,18 +1,15 @@
+
+# Object-Oriented Programming (OOP) in Python Part-1
 class Car:
     def __init__(self, owner):
         print("Car is being initialized...")
-        self.owmer = owner
+        self.owner = owner
         self.color = "red"
         self.brand = "Toyota"
         print("Car created")
 
 car1 = Car('Arbin')
-print(f"Owner: {car1.owmer}")
+print(f"Owner: {car1.owner}")
 print(car1.color)
 print(car1.brand)
-
-car2 = Car('John')
-print(f"Owner: {car2.owmer}")
-print(car2.color)
-print(car2.brand)
 
