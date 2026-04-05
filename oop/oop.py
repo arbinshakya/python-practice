adbb01125dde61e09a88c227533dcb6ac13488bb
Arbin <arbinshakya123@gmail.com>
2026-04-05 21:41:00 +0545

    new parameters

--- oop/oop.py
+++ oop/oop.py
@@ -1,14 +1,14 @@
 
 # Object-Oriented Programming (OOP) in Python Part-1
 class Car:
-    def __init__(self, owner):
+    def __init__(self, owner, color, brand):
         print("Car is being initialized...")
         self.owner = owner
-        self.color = "red"
-        self.brand = "Toyota"
+        self.color = color
+        self.brand = brand
         print("Car created")
 
-car1 = Car('Arbin') 
+car1 = Car('Arbin', 'Red', 'Toyota') 
 print(f"Owner: {car1.owner}")
 print(car1.color)
 print(car1.brand)
